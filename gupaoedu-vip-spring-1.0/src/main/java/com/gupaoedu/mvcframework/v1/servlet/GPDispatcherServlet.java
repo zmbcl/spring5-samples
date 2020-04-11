517b61f1eab49f86f6ed1e61268b9a556bf38755
bcl <1010714847@qq.com>
2020-04-11 20:26:02 +0800

    思路整理

--- gupaoedu-vip-spring-1.0/src/main/java/com/gupaoedu/mvcframework/v1/servlet/GPDispatcherServlet.java
+++ gupaoedu-vip-spring-1.0/src/main/java/com/gupaoedu/mvcframework/v1/servlet/GPDispatcherServlet.java
@@ -1,8 +1,10 @@
 package com.gupaoedu.mvcframework.v1.servlet;
+
 import com.gupaoedu.mvcframework.annotation.GPAutowired;
 import com.gupaoedu.mvcframework.annotation.GPController;
 import com.gupaoedu.mvcframework.annotation.GPRequestMapping;
 import com.gupaoedu.mvcframework.annotation.GPService;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,40 +19,54 @@ import java.net.URL;
 import java.util.*;
 
 public class GPDispatcherServlet extends HttpServlet {
-    private Map<String,Object> mapping = new HashMap<String, Object>();
+    private Map<String, Object> mapping = new HashMap<String, Object>();
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {this.doPost(req,resp);}
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            doDispatch(req,resp);
+            doDispatch(req, resp);
         } catch (Exception e) {
             resp.getWriter().write("500 Exception " + Arrays.toString(e.getStackTrace()));
         }
     }
+
     private void doDispatch(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         String url = req.getRequestURI();
         String contextPath = req.getContextPath();
         url = url.replace(contextPath, "").replaceAll("/+", "/");
-        if(!this.mapping.containsKey(url)){resp.getWriter().write("404 Not Found!!");return;}
+        if (!this.mapping.containsKey(url)) {
+            resp.getWriter().write("404 Not Found!!");
+            return;
+        }
         Method method = (Method) this.mapping.get(url);
-        Map<String,String[]> params = req.getParameterMap();
-        method.invoke(this.mapping.get(method.getDeclaringClass().getName()),new Object[]{req,resp,params.get("name")[0]});
+        Map<String, String[]> params = req.getParameterMap();
+        method.invoke(this.mapping.get(method.getDeclaringClass().getName()), new Object[]{req, resp, params.get("name")[0]});
     }
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         InputStream is = null;
-        try{
+        try {
             Properties configContext = new Properties();
             is = this.getClass().getClassLoader().getResourceAsStream(config.getInitParameter("contextConfigLocation"));
             configContext.load(is);
+            // 获取配置文件中需要扫描的包
             String scanPackage = configContext.getProperty("scanPackage");
+            // 遍历配置文件中包下的所有类，并将全路径为键值为null保存到mapping
             doScanner(scanPackage);
             for (String className : mapping.keySet()) {
-                if(!className.contains(".")){continue;}
+                if (!className.contains(".")) {
+                    continue;
+                }
                 Class<?> clazz = Class.forName(className);
-                if(clazz.isAnnotationPresent(GPController.class)){
-                    mapping.put(className,clazz.newInstance());
+                // isAnnotationPresent判断是否有此注解
+                if (clazz.isAnnotationPresent(GPController.class)) {
+                    mapping.put(className, clazz.newInstance());
                     String baseUrl = "";
                     if (clazz.isAnnotationPresent(GPRequestMapping.class)) {
                         GPRequestMapping requestMapping = clazz.getAnnotation(GPRequestMapping.class);
@@ -58,36 +74,51 @@ public class GPDispatcherServlet extends HttpServlet {
                     }
                     Method[] methods = clazz.getMethods();
                     for (Method method : methods) {
-                        if (!method.isAnnotationPresent(GPRequestMapping.class)) {  continue; }
+                        if (!method.isAnnotationPresent(GPRequestMapping.class)) {
+                            continue;
+                        }
                         GPRequestMapping requestMapping = method.getAnnotation(GPRequestMapping.class);
                         String url = (baseUrl + "/" + requestMapping.value()).replaceAll("/+", "/");
                         mapping.put(url, method);
                         System.out.println("Mapped " + url + "," + method);
                     }
-                }else if(clazz.isAnnotationPresent(GPService.class)){
-                        GPService service = clazz.getAnnotation(GPService.class);
-                        String beanName = service.value();
-                        if("".equals(beanName)){beanName = clazz.getName();}
-                        Object instance = clazz.newInstance();
-                        mapping.put(beanName,instance);
-                        for (Class<?> i : clazz.getInterfaces()) {
-                            mapping.put(i.getName(),instance);
-                        }
-                }else {continue;}
+                } else if (clazz.isAnnotationPresent(GPService.class)) {
+                    GPService service = clazz.getAnnotation(GPService.class);
+                    String beanName = service.value();
+                    if ("".equals(beanName)) {
+                        beanName = clazz.getName();
+                    }
+                    Object instance = clazz.newInstance();
+                    mapping.put(beanName, instance);
+                    // getInterfaces获取该类实现的所有接口
+                    for (Class<?> i : clazz.getInterfaces()) {
+                        mapping.put(i.getName(), instance);
+                    }
+                } else {
+                    continue;
+                }
             }
+            // map.values获取Map中所有值
             for (Object object : mapping.values()) {
-                if(object == null){continue;}
+                if (object == null) {
+                    continue;
+                }
                 Class clazz = object.getClass();
-                if(clazz.isAnnotationPresent(GPController.class)){
-                    Field [] fields = clazz.getDeclaredFields();
+                if (clazz.isAnnotationPresent(GPController.class)) {
+                    Field[] fields = clazz.getDeclaredFields();
                     for (Field field : fields) {
-                        if(!field.isAnnotationPresent(GPAutowired.class)){continue; }
+                        if (!field.isAnnotationPresent(GPAutowired.class)) {
+                            continue;
+                        }
                         GPAutowired autowired = field.getAnnotation(GPAutowired.class);
                         String beanName = autowired.value();
-                        if("".equals(beanName)){beanName = field.getType().getName();}
+                        if ("".equals(beanName)) {
+                            beanName = field.getType().getName();
+                        }
+                        // setAccessible设置field可以通过反射访问到
                         field.setAccessible(true);
                         try {
-                            field.set(mapping.get(clazz.getName()),mapping.get(beanName));
+                            field.set(mapping.get(clazz.getName()), mapping.get(beanName));
                         } catch (IllegalAccessException e) {
                             e.printStackTrace();
                         }
@@ -95,23 +126,36 @@ public class GPDispatcherServlet extends HttpServlet {
                 }
             }
         } catch (Exception e) {
-        }finally {
-            if(is != null){
-                try {is.close();} catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
         System.out.print("GP MVC Framework is init");
     }
+
+    /**
+     * @Description: 遍历配置文件中包下的所有类，并将全路径为键值为null保存到mapping
+     * @Author: bcl
+     * @Date: 2020/4/11 7:38 下午
+     */
     private void doScanner(String scanPackage) {
-        URL url = this.getClass().getClassLoader().getResource("/" + scanPackage.replaceAll("\\.","/"));
+        URL url = this.getClass().getClassLoader().getResource("/" + scanPackage.replaceAll("\\.", "/"));
         File classDir = new File(url.getFile());
         for (File file : classDir.listFiles()) {
-            if(file.isDirectory()){ doScanner(scanPackage + "." +  file.getName());}else {
-                if(!file.getName().endsWith(".class")){continue;}
-                String clazzName = (scanPackage + "." + file.getName().replace(".class",""));
-                mapping.put(clazzName,null);
+            if (file.isDirectory()) {
+                doScanner(scanPackage + "." + file.getName());
+            } else {
+                if (!file.getName().endsWith(".class")) {
+                    continue;
+                }
+                String clazzName = (scanPackage + "." + file.getName().replace(".class", ""));
+                mapping.put(clazzName, null);
             }
         }
     }
