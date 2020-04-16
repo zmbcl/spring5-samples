b06eff399a55abbc808d35f5be8329d46ebfde68
bcl <1010714847@qq.com>
2020-04-16 15:56:58 +0800

    设计模式——简单工厂：如果我们的业务继续拓展，要增加前端课程，那么工厂中的create方法就要每次都根据产品的增加修改代码逻辑，不符合开闭原则，因此继续优化

--- design_patterns/src/main/java/org/apache/patterns/factory01/CourseFactory.java
+++ design_patterns/src/main/java/org/apache/patterns/factory01/CourseFactory.java
@@ -6,7 +6,7 @@ package org.apache.patterns.factory01;
  * @Date: Create in 3:44 下午 2020/4/16
  */
 public class CourseFactory {
-    public ICourse create(String name) {
+    /*public ICourse create(String name) {
         if ("java".equals(name)) {
             return new JavaCourse();
         } else if ("python".equals(name)) {
@@ -14,5 +14,21 @@ public class CourseFactory {
         } else {
             return null;
         }
+    }*/
+    // 如果我们的业务继续拓展，要增加前端课程，
+    // 那么工厂中的create方法就要每次都根据产品的增加修改代码逻辑，不符合开闭原则，
+    // 因此继续优化如下
+    public ICourse create(String className) {
+        try {
+            return (ICourse) Class.forName(className).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 }
--- design_patterns/src/main/java/org/apache/patterns/factory01/Test_SimpleFactory.java
+++ design_patterns/src/main/java/org/apache/patterns/factory01/Test_SimpleFactory.java
@@ -7,8 +7,12 @@ package org.apache.patterns.factory01;
  */
 public class Test_SimpleFactory {
     public static void main(String[] args) {
-        CourseFactory factory = new CourseFactory();
+        /*CourseFactory factory = new CourseFactory();
         ICourse iCourse = factory.create("java");
+        iCourse.record();*/
+        // 优化之后符合开闭原则的调用
+        CourseFactory factory = new CourseFactory();
+        ICourse iCourse = factory.create("org.apache.patterns.factory01.PythonCourse");
         iCourse.record();
     }
 }
