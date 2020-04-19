140bab6f3fd9cf62dc5b1c7a6e35ebecadd3dcaf
bcl <1010714847@qq.com>
2020-04-19 13:50:45 +0800

    设计模式——单例模式（懒汉式synchronized上锁提升性能问题）

--- design_patterns/src/main/java/org/apache/patterns/singleton02/SingletonLazySimple.java
+++ design_patterns/src/main/java/org/apache/patterns/singleton02/SingletonLazySimple.java
@@ -13,9 +13,13 @@ public class SingletonLazySimple {
 
     // 用synchronized加锁时，在线程数量比较多的情况下，
     // 如果CPU分配压力上升，则会导致大批线程阻塞，从而导致程序性能大幅下降
-    public synchronized static SingletonLazySimple getInstance() {
+    public static SingletonLazySimple getInstance() {
         if (singletonLazySimple == null) {
-            singletonLazySimple = new SingletonLazySimple();
+            synchronized (SingletonLazySimple.class){
+                if (singletonLazySimple==null){
+                    singletonLazySimple = new SingletonLazySimple();
+                }
+            }
         }
         return singletonLazySimple;
     }
