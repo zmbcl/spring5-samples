7e52c1522036550e441e927277008891c1667208
bcl <1010714847@qq.com>
2020-04-21 15:22:37 +0800

    设计模式——代理模式 动态代理 CGLib代理原理解析

--- design_patterns/src/main/java/org/apache/patterns/proxy07/Test_Cglib.java
+++ design_patterns/src/main/java/org/apache/patterns/proxy07/Test_Cglib.java
@@ -1,5 +1,7 @@
 package org.apache.patterns.proxy07;
 
+import net.sf.cglib.core.DebuggingClassWriter;
+
 /**
  * @Auther: bcl
  * @Description:
@@ -7,7 +9,13 @@ package org.apache.patterns.proxy07;
  */
 public class Test_Cglib {
     public static void main(String[] args) throws Exception {
+        // 利用CGLib的代理类可以将内存中的.class文件写入本地磁盘
+        // 会发现根目录下多了三个.class文件，Customer$$EnhancerByCGLIB$$48dfeb3.class就是生成的代理类，集成了Customer类
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "cglib_proxy_class");
+
         Customer obj = (Customer) new CGLibMeipo().getInstance(Customer.class);
         obj.findLove();
     }
+
+
 }
